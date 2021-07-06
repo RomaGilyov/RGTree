1e93a56a63e28679297b6b1b5936ff643b1e16b4
RomaGilyov <rgilyov@gmail.com>
2021-07-06 20:12:11 +0300

    comments tranverse

--- comments/comments.go
+++ comments/comments.go
@@ -1,5 +1,10 @@
 package comments
 
+import (
+	"errors"
+	"strconv"
+)
+
 type Comment interface {
 	SetChild(Comment)
 	GetChildren() []Comment
@@ -19,7 +24,7 @@ func makeTreeUtil(root Comment, comments []Comment, memo map[int]bool) Comment {
 	var children []Comment
 
 	for _, child := range comments {
-		if child.GetParentID() == root.GetID() && memo[child.GetID()] != false {
+		if child.GetParentID() == root.GetID() && memo[child.GetID()] == false {
 			memo[child.GetID()] = true
 
 			children = append(children, child)
@@ -33,22 +38,28 @@ func makeTreeUtil(root Comment, comments []Comment, memo map[int]bool) Comment {
 	return root
 }
 
-func Traverse(root *Comment, handler func(*Comment)) {
+func Traverse(root Comment, handler func(Comment)) error {
 	memo := make(map[int]bool, 10)
 
-	traverseUtil(root, handler, memo)
+	return traverseUtil(root, handler, memo)
 }
 
-func traverseUtil(root *Comment, handler func(*Comment), memo map[int]bool) {
-	if memo[(*root).GetID()] == true {
-		return
+func traverseUtil(root Comment, handler func(Comment), memo map[int]bool) error {
+	if memo[root.GetID()] == true {
+		return errors.New("there are duplicate primary ID elements of value: " + strconv.Itoa(root.GetID()))
 	}
 
 	handler(root)
 
-	memo[(*root).GetID()] = true
+	memo[root.GetID()] = true
 
-	for _, child := range (*root).GetChildren() {
-		Traverse(&child, handler)
+	for _, child := range root.GetChildren() {
+		err := traverseUtil(child, handler, memo)
+
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
--- comments/comments_test.go
+++ comments/comments_test.go
@@ -1,13 +1,16 @@
 package comments
 
 import (
-	"fmt"
+	"strconv"
 	"testing"
 )
 
+////////////////////////////////////////////////////// Testing data //////////////////////////////////////
+
 type CommentTest struct {
+	Title string
 	ID int
-	ParentId int
+	ParentID int
 	Children []Comment
 }
 
@@ -24,25 +27,84 @@ func (ct CommentTest) GetID() int {
 }
 
 func (ct CommentTest) GetParentID() int {
-	return ct.ParentId
+	return ct.ParentID
 }
 
-func TestMakeTreeMap(t *testing.T) {
+func (ct CommentTest) String() string {
+	return "{ID: " + strconv.Itoa(ct.ID) + ", ParentID: " + strconv.Itoa(ct.ParentID) + ", Title: " + ct.Title + "}"
+}
+
+/*
+	There are test comments with this structure:
+
+		1 ->
+			2 ->
+				4
+			3 ->
+				5
+ */
+func testData() (Comment, []Comment) {
 	var plain []Comment
 
-	root := &CommentTest{ID: 1}
+	root := &CommentTest{ID: 1, ParentID: 3, Title: "Test 1"} // ParentID 3 -> Check for infinite recursion
 
 	plain = append(plain, root)
-	plain = append(plain, &CommentTest{ID: 2, ParentId: 1})
-	plain = append(plain, &CommentTest{ID: 3, ParentId: 1})
-	plain = append(plain, &CommentTest{ID: 4, ParentId: 2})
-	plain = append(plain, &CommentTest{ID: 5, ParentId: 2})
-	plain = append(plain, &CommentTest{ID: 6, ParentId: 2})
-	plain = append(plain, &CommentTest{ID: 7, ParentId: 3})
-	plain = append(plain, &CommentTest{ID: 8, ParentId: 3})
-	plain = append(plain, &CommentTest{ID: 9, ParentId: 4})
-	plain = append(plain, &CommentTest{ID: 10, ParentId: 4})
+	plain = append(plain, &CommentTest{ID: 2, ParentID: 1, Title: "Test 2"})
+	plain = append(plain, &CommentTest{ID: 3, ParentID: 1, Title: "Test 3"})
+	plain = append(plain, &CommentTest{ID: 4, ParentID: 2, Title: "Test 4"})
+	plain = append(plain, &CommentTest{ID: 5, ParentID: 3, Title: "Test 5"})
 
-	fmt.Println(MakeTree(root, plain))
+	return root, plain
 }
 
+////////////////////////////////////////////////////// Testing /////////////////////////////////////////
+
+func TestMakeTreeMap(t *testing.T) {
+	root, plain := testData()
+
+	MakeTree(root, plain)
+
+	if len(root.GetChildren()) != 2 {
+		t.Fatalf("%s must have 2 children", root)
+	}
+
+	if root.GetChildren()[0].GetChildren()[0].GetID() != 4 {
+		t.Fatalf("%s ->children -> 0 -> children -> 0 -> ID must be 4", root)
+	}
+
+	if root.GetChildren()[1].GetChildren()[0].GetID() != 5 {
+		t.Fatalf("%s ->children -> 1 -> children -> 0 -> ID must be 5", root)
+	}
+}
+
+var TestTraverseIterations int
+
+func TestTraverse(t *testing.T) {
+	root, plain := testData()
+
+	MakeTree(root, plain)
+
+	err := Traverse(root, func (comment Comment) {
+		if ct, ok := comment.(*CommentTest); ok {
+			ct.Title = "Test " + strconv.Itoa(ct.GetID()) + " Modified"
+		}
+
+		TestTraverseIterations++
+	})
+
+	if TestTraverseIterations != 5 {
+		t.Fatal("There are 5 elements", TestTraverseIterations, "given")
+	}
+
+	if err != nil {
+		t.Fatal("There are no duplicates but there duplicate error")
+	}
+
+	root.GetChildren()[0].SetChild(&CommentTest{ID: 2}) // Check for doubles
+
+	err = Traverse(root, func (comment Comment) {})
+
+	if err == nil {
+		t.Fatal("Traverse function must give error when a duplicate encountered")
+	}
+}
