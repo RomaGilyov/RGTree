162fe7c7bfe58dd93fa14eb70ebab684bf8fe14a
RomaGilyov <rgilyov@gmail.com>
2021-08-24 17:47:32 +0300

    trie

--- trie/trie.go
+++ trie/trie.go
@@ -1,6 +1,8 @@
 package trie
 
-import "fmt"
+import (
+	"errors"
+)
 
 /*
 	Trie
@@ -17,17 +19,13 @@ func Construct() *Trie {
 func (t *Trie) PrefixSearch(prefix []interface{}) [][]interface{} {
 	results := make([][]interface{}, 0, 10)
 
-	if t.Exists(prefix) == false {
+	suffix, err := t.GetSuffix(prefix)
+
+	if err != nil {
 		return results
 	}
 
-	suffix := t.getSuffixUtil(prefix)
-
-	fmt.Println(suffix)
-
-	results = ToDict(suffix)
-
-	fmt.Println(results)
+	results = suffix.Flatten()
 
 	for i, col := range results {
 		results[i] = append(prefix, col...)
@@ -56,36 +54,42 @@ func (t *Trie) PrefixSearch(prefix []interface{}) [][]interface{} {
 	1 3 6
 	1 3 7
  */
-func ToDict(node *Trie) [][]interface{} {
-	return trieToDictUtil(node, make([]interface{}, 0, 10), make([][]interface{}, 0, 10))
-}
+func (t *Trie) Flatten() [][]interface{} {
+	results := make([][]interface{}, 0, 10)
 
-func trieToDictUtil(node *Trie, path []interface{}, results [][]interface{}) [][]interface{} {
-	if len(node.Children) == 0 {
-		fmt.Println(path)
-
-		return append(results, path)
-	} else {
-		for _, child := range node.Children {
-			results = trieToDictUtil(child, append(path, child.Val), results)
-		}
-	}
+	t.trieToDictUtil(make([]interface{}, 0, 10), &results)
 
 	return results
 }
 
-func (t *Trie) getSuffixUtil(values []interface{}) *Trie {
+func (t *Trie) trieToDictUtil(path []interface{}, results *[][]interface{}) {
+	if len(t.Children) == 0 {
+		clonePath := make([]interface{}, 0)
+
+		for _, v := range path {
+			clonePath = append(clonePath, v)
+		}
+
+		*results = append(*results, clonePath)
+	} else {
+		for _, child := range t.Children {
+			child.trieToDictUtil(append(path, child.Val), results)
+		}
+	}
+}
+
+func (t *Trie) GetSuffix(values []interface{}) (*Trie, error) {
 	if len(values) == 0 {
-		return t
+		return t, nil
 	}
 
 	val := values[0]
 
 	if child, ok := t.Children[val]; ok {
-		return child.getSuffixUtil(values[1:])
+		return child.GetSuffix(values[1:])
 	}
 
-	panic("Exists but was not found")
+	return nil, errors.New("suffix does not exists")
 }
 
 func (t *Trie) Exists(values []interface{}) bool {
@@ -119,5 +123,29 @@ func (t *Trie) Add(values []interface{}) {
 		t.Children[val] = &Trie{Val: val, Children: make(map[interface{}]*Trie)}
 
 		t.Children[val].Add(values[1:])
+	}
+}
+
+func (t *Trie) Remove(values []interface{}) {
+	if len(values) == 0 {
+		return
+	}
+
+	suffixParent, pErr := t.GetSuffix(values[:len(values) - 1])
+
+	if pErr != nil {
+		return
+	}
+
+	suffix, err := t.GetSuffix(values)
+
+	if err != nil {
+		return
+	}
+
+	if len(suffix.Children) == 0 {
+		delete(suffixParent.Children, suffix.Val)
+
+		t.Remove(values[:len(values) - 1])
 	}
 }
--- trie/trie_test.go
+++ trie/trie_test.go
@@ -1,19 +1,142 @@
 package trie
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestPrefixSearch(t *testing.T) {
+func testTrie() *Trie {
 	trie := Construct()
 
 	trie.Add([]interface{}{"c", "a", "r"})
 	trie.Add([]interface{}{"c", "a", "t"})
 	trie.Add([]interface{}{"c", "a", "r", "s"})
 	trie.Add([]interface{}{"c", "a", "r", "d"})
+	trie.Add([]interface{}{"d", "a", "y"})
+	trie.Add([]interface{}{"w", "i", "n"})
+	trie.Add([]interface{}{"w", "i", "n", "d", "o", "w"})
+	trie.Add([]interface{}{"c", "l", "o", "s", "e"})
+	trie.Add([]interface{}{"i", "m", "l", "e", "m", "e", "n", "t", "a", "t", "i", "n"})
 
-	fmt.Println(trie.PrefixSearch([]interface{}{}))
-	fmt.Println(trie.Exists([]interface{}{"c", "r"}))
-	//fmt.Println(trie.Children["c"].Children["a"].Children["r"])
+	return trie
+}
+
+//func testDict() [][]interface{} { todo
+//	dict := []string{
+//		"cat",
+//		"dog",
+//		"car",
+//		"ball",
+//		"analogy",
+//		"dufference",
+//		"dufference",
+//	}
+//}
+
+func TestAdd(t *testing.T) {
+	trie := testTrie()
+
+	trie.Add([]interface{}{"d", "o", "o", "r"})
+
+	if _, ok := trie.Children["d"].Children["o"].Children["o"].Children["r"]; ! ok {
+		t.Fatal("must have word door")
+	}
+}
+
+func TestExists(t *testing.T) {
+	trie := testTrie()
+
+	if trie.Exists([]interface{}{"w", "i", "n"}) == false {
+		t.Fatal("word win exists in the trie")
+	}
+}
+
+func TestSuffix(t *testing.T) {
+	trie := testTrie()
+
+	s, err := trie.GetSuffix([]interface{}{"w", "i", "n"})
+
+	if err != nil {
+		t.Fatal("must have suffix node to win")
+	}
+
+	if s.Val != "n" {
+		t.Fatal("suffix node must be `n`")
+	}
+}
+
+func TestPrefixSearch(t *testing.T) {
+	trie := testTrie()
+
+	dict := trie.PrefixSearch([]interface{}{"c", "a"})
+
+	if len(dict) != 3 {
+		t.Fatal("dict must have 3 words")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	trie := testTrie()
+
+	trie.Remove([]interface{}{"w", "i", "n"})
+
+	if _, ok := trie.Children["w"].Children["i"].Children["n"]; ! ok {
+		t.Fatal("trie can not remove win word because it is prefix of word window")
+	}
+
+	trie.Remove([]interface{}{"w", "i", "n", "d", "o", "w"})
+
+	if _, ok := trie.Children["w"]; ok {
+		t.Fatal("window word must be removed")
+	}
+}
+
+func TestFlatten(t *testing.T) {
+	trie := testTrie()
+
+	dict := trie.Flatten()
+
+	if len(dict) != 7 {
+		t.Fatal("must have 7 words")
+	}
+}
+
+func BenchmarkExists(b *testing.B) {
+	trie := testTrie()
+
+	for n := 0; n < b.N; n++ {
+		trie.Exists([]interface{}{"w", "i", "n"})
+	}
+}
+
+func BenchmarkExistsPlainDict(b *testing.B) {
+	trie := testTrie()
+
+	dict := trie.Flatten()
+
+	needle := []interface{}{"w", "i", "n"}
+
+	for n := 0; n < b.N; n++ {
+		plainSearchBench(needle, dict)
+	}
+}
+
+func plainSearchBench(needle []interface{}, haystack [][]interface{}) bool {
+	for _, word := range haystack {
+		found := false
+
+		for i, l := range needle {
+			if word[i] != l {
+				found = false
+				break
+			} else {
+				found = true
+			}
+		}
+
+		if found == true {
+			return true
+		}
+	}
+
+	return false
 }
